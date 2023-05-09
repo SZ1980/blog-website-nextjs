0a813609d5e107b9d0f06e17b09309c7c5c148da
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-09 13:51:18 +0200

    revalidate start

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
 import Posts from '@/components/Posts'
 
-const inter = Inter({ subsets: ['latin'] })
+export const revalidate = 10
 
 export default function Home() {
   return (
